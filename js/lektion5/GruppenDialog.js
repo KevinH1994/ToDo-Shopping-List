b1ec6df8adcf88619fc59096d78089d16f5bf1c9
Coach @lfred <alfred.walther@artingo.net>
2022-04-05 08:59:59 +0200

    Lektion 5: Lösung hochgeladen

---
+++ js/lektion5/GruppenDialog.js
@@ -0,0 +1,75 @@
+class GruppenDialog extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDialog: this.props.visible,
+      gruppenListe: this.props.gruppenListe,
+    };
+  }
+
+  gruppeHinzufuegen = () => {
+    let eingabe = document.getElementById("eingabe")
+    if (eingabe.value.trim().length > 0) {
+      App.gruppeHinzufuegen(eingabe.value)
+      this.setState({gruppenListe: App.gruppenListe})
+    }
+    eingabe.value = ""
+    eingabe.focus()
+  }
+
+  gruppeBearbeiten = (gruppe) => {
+    let eingabe = document.getElementById("eingabe")
+    if (eingabe.value.trim().length > 0) {
+      App.gruppeUmbenennen(gruppe, eingabe.value)
+      this.setState({gruppenListe: App.gruppenListe})
+    }
+    eingabe.value = ""
+    eingabe.focus()
+  }
+
+  gruppeEntfernen = (gruppe) => {
+    App.gruppeEntfernen(gruppe)
+    this.setState({gruppenListe: App.gruppenListe})
+  }
+
+  render() {
+    return (
+      <div className={'mdc-dialog ' + (this.props.visible ? 'mdc-dialog--open' : '')}>
+        <div className="mdc-dialog__container">
+          <div className="mdc-dialog__surface">
+            <h2 className="mdc-dialog__title">Gruppen bearbeiten</h2>
+
+            <div className="mdc-dialog__content">
+              <nav>
+                <input type="text" id="eingabe" placeholder="Gruppe hinzufügen"
+                       onKeyUp={event => (event.keyCode == 13) ? this.gruppeHinzufuegen() : ''}/>
+                <button className="material-icons"
+                        onClick={() => this.gruppeHinzufuegen()}>add_circle</button>
+              </nav>
+              <hr/>
+
+              <dl className="mdc-deprecated-list">
+                {this.state.gruppenListe.map(gruppe => (
+                  <dt key={gruppe.id}>
+                    <span>{gruppe.name}</span>
+                    <i className="material-icons" onClick={()=>this.gruppeBearbeiten(gruppe.id)}>
+                      drive_file_rename_outline</i>
+                    <i className="material-icons" onClick={()=>this.gruppeEntfernen(gruppe.id)}>delete</i>
+                  </dt>
+                ))}
+              </dl>
+            </div>
+
+            <div className="mdc-dialog__actions">
+              <button type="button" className="mdc-button mdc-dialog__button"
+                      onClick={()=>this.props.onDialogClose()}>
+                <span className="mdc-button__label">Schließen</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
